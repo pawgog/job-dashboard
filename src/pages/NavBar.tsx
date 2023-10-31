3affcaf70f2c90a48d8927444689b820c8087cb2
pawgog <pgogola10@gmail.com>
2023-10-31 22:02:45 +0100

    Add hamburger input

--- src/pages/NavBar.tsx
+++ src/pages/NavBar.tsx
@@ -5,6 +5,8 @@ const NavBar = () => {
     <S.NavBar>
       <div className="logo">Logo</div>
       <ul>
+        <input type="checkbox" id="checkbox_toggle" />
+        <label htmlFor="checkbox_toggle" className="hamburger">&#9776;</label>
         <li>Home</li>
         <li>Login</li>
       </ul>
