fe47a813cd270b6d8ccf508b4b489c60d256ffb0
pawgog <pgogola10@gmail.com>
2023-11-09 18:21:55 +0100

    NavBar styles

--- src/pages/NavBar/NavBar.tsx
+++ src/pages/NavBar/NavBar.tsx
@@ -6,9 +6,13 @@ const NavBar = () => {
       <div className="logo">Logo</div>
       <ul>
         <input type="checkbox" id="checkbox_toggle" />
-        <label htmlFor="checkbox_toggle" className="hamburger">&#9776;</label>
-        <li>Home</li>
-        <li>Login</li>
+        <label htmlFor="checkbox_toggle" className="hamburger">
+          &#9776;
+        </label>
+        <S.NavMenu>
+          <li>Home</li>
+          <li>Login</li>
+        </S.NavMenu>
       </ul>
     </S.NavBar>
   );
