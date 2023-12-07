064770fe34b5568ac39bb223c65ab944a725d8ce
pawgog <pgogola10@gmail.com>
2023-12-07 20:56:47 +0100

    Add react query library

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 import NavBar from './pages/NavBar/NavBar.tsx';
 import Dashboard from './pages/Dashboard/Dashboard.tsx';
 import './index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NavBar />
-    <Dashboard />
+    <QueryClientProvider client={queryClient}>
+      <NavBar />
+      <Dashboard />
+    </QueryClientProvider>
   </React.StrictMode>
 );
