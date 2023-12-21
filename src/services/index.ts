231aaa7712ed9f77a82e36ef1c6f1599f0382e37
pawgog <pgogola10@gmail.com>
2023-12-21 18:08:10 +0100

    Create axios services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,18 @@
-export const fetchTasks = async () => {
-  const res = await fetch("http://localhost:3000/tasks");
-  return res.json();
-};
+import axios from "axios";
+
+export async function fetchTasks() {
+  try {
+    const tasks = await axios.get("http://localhost:3000/tasks");
+    return tasks.data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function createTask(task: string) {
+  try {
+    await axios.post(`http://localhost:3000/tasks/`, { data: task });
+  } catch (e) {
+    console.log(e);
+  }
+}
