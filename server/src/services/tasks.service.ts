8707cd1413fb734a229612d90204ad2cedb57858
pawgog <pgogola10@gmail.com>
2024-02-08 18:55:52 +0100

    Update fetching all tasks to js v3

--- server/src/services/tasks.service.ts
+++ server/src/services/tasks.service.ts
@@ -1,34 +1,40 @@
-import { dynamodb, TABLE_NAME } from "../config/dynamo"
+// import { GetCommand, PutCommand } from "@aws-sdk/lib-dynamodb";
+import { ScanCommand } from "@aws-sdk/client-dynamodb";
+import { docClient, TABLE_NAME } from "../config/dynamo"
+
+type Items = {
+  id: number;
+  name: string;
+  column: string;
+};
 
 const getTasks = async () => {
   try {
+    const params = new ScanCommand({
+        TableName: TABLE_NAME,
+    });
+
+    const result = await docClient.send(params);
+
+    return result;
+  } catch (err: any) {
+    throw new Error(err.message);
+  }
+
+}
+const addOrUpdateTasks = async (item: Object) => {
+  try {
       const params = {
-          TableName: TABLE_NAME
+          TableName: TABLE_NAME,
+          Item: item
       };
 
-      const result = await dynamodb.scan(params).promise();
+      const result = await dynamodb.put(params).promise();
 
       return result;
   } catch (err: any) {
       throw new Error(err.message);
   }
-
 }
 
-const addOrUpdateTasks = async (item: Object) => {
-    try {
-        const params = {
-            TableName: TABLE_NAME,
-            Item: item
-        };
-
-        const result = await dynamodb.put(params).promise();
-
-        return result;
-    } catch (err: any) {
-        throw new Error(err.message);
-    }
-}
-
-
-export { getTasks, addOrUpdateTasks }
+export { getTasks, getTaskById, updateTask }
