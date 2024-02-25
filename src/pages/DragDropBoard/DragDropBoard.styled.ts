f761a049922d045c19908e6998d15c26fcd7db90
pawgog <pgogola10@gmail.com>
2024-02-25 12:32:53 +0100

    Move function to helper file and dragdrop styles

--- src/pages/DragDropBoard/DragDropBoard.styled.ts
+++ src/pages/DragDropBoard/DragDropBoard.styled.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { colors } from '../../global/colors';
 
 export const Board = styled.div`
@@ -25,7 +25,32 @@ export const ColumTitle = styled.p`
   font-weight: 700;
 `;
 
+export const ColumnBody = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+
+  & input {
+    padding: 0.3rem 0.3rem 0.3rem 0.7rem;
+    line-height: 1.5rem;
+    font-size: 0.8rem;
+    font-weight: 500;
+    font-family: inherit;
+    max-width: 100px;
+    margin-left: 1rem;
+    border: 2px solid ${colors.lightGrey};
+    border-radius: .5rem;
+    transition: border .3s ease;
+
+    &:focus {
+        outline: none;
+        border-color: ${colors.violet};
+    }
+  }
+`;
+
 export const Item = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -35,24 +60,60 @@ export const Item = styled.div`
   border-radius: 0.5rem;
   background-color: ${colors.itemBackground};
   box-shadow: 0px 0px 3px rgba(0, 0, 0, 0.4);
+
+  &:hover {
+    button {
+      opacity: 1;
+    }
+  }
 `
 
 export const Button = styled.button`
   display: flex;
   align-items: center;
-  margin-bottom: 0.5rem;
-  padding: 0.4rem 0.8rem;
   border-radius: 0.7rem;
   border: none;
+  background: none;
 
-  & input {
-    max-width: 100px;
-    margin-right: 0.5rem;
-    border: none;
-    box-shadow: 0px 0 5px 1px ${colors.lightGrey};
+  &:hover {
+    color: ${colors.violet};
   }
+
   & svg {
     cursor: pointer;
     font-size: 1.5rem;
   }
+`;
+
+const ButtomActionStyled = css`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  top: -0.8rem;
+  padding: 0.2rem 0.2rem;
+  opacity: 0;
+  border-radius: 1rem;
+  background: none;
+  border: 1px solid transparent;
+  transition: opacity 0.3s ease-out;
+
+  &:hover {
+    border: 1px solid black;
+  }
+
+  & svg {
+    cursor: pointer;
+    font-size: 1.3rem;
+  }
+`;
+
+export const ButtonDelete = styled.button`
+  ${ButtomActionStyled}
+  right: -0.2rem;
+`;
+
+export const ButtonEdit = styled.button`
+  ${ButtomActionStyled}
+  right: 1.3rem;
 `;
---
+++ src/utils/helper.ts
@@ -0,0 +1,21 @@
+import { ItemsArray } from "./types";
+
+type Props = {
+  data: ItemsArray[];
+  prop: string;
+  value: string;
+  comparedValue: number | string;
+  changedValue: string;
+}
+
+export const changeItem = ({data, prop, value, comparedValue, changedValue}: Props) => {
+  const newItems = data
+    .filter((item: ItemsArray) => item[prop as keyof typeof item] === comparedValue)
+    .map((item: ItemsArray) => {
+      return {
+        ...item,
+        [value]: changedValue
+      };
+    });
+  return { newItems };
+};
