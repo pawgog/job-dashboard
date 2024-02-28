052e9ceee5220b237aa7e1d63703febef7c940a5
pawgog <pgogola10@gmail.com>
2024-02-28 20:34:09 +0100

    New service integration in API

--- src/services/index.ts
+++ src/services/index.ts
@@ -8,32 +8,40 @@ export async function fetchTasks() {
   try {
     const { data } = await axios.get(`${awsPath}/dev/tasks`);
     return data?.result;
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.error('An error occurred:', error);
   }
 }
 
 export async function fetchTaskById(id: number) {
   try {
     await axios.get(`${awsPath}/dev/tasks/${id}`);
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.error('An error occurred:', error);
   }
 }
 
-export async function createTask(task: string) {
+export async function createTask(task: ItemsArray) {
   try {
     await axios.post(`${awsPath}/dev/tasks`, { data: task });
-  } catch (e) {
-    console.log(e);
+  } catch (error) {
+    console.error('An error occurred:', error);
   }
 }
 
 export async function updateTask(items: ItemsArray[]) {
   const [tasks] = items;
   try {
-    await axios.post(`${awsPath}/dev/tasks`, { data: tasks });
-  } catch (e) {
-    console.log(e);
+    await axios.put(`${awsPath}/dev/tasks`, { data: tasks });
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
+
+export async function deleteTask(id: string) {
+  try {
+    await axios.delete(`${awsPath}/dev/tasks/${id}`);
+  } catch (error) {
+    console.error('An error occurred:', error);
   }
 }
